7b8a557229d9a4a86b0582e3d3adc96413062878
ruyalabs <dev@ruyalabs.ch>
2025-06-22 17:26:54 +0200

    Validate incoming `PaymentDto` in `PaymentRequestConsumer` using `@Valid` and update method to include `@Payload`.

--- src/main/java/ch/ruyalabs/springkafkalab/consumer/PaymentRequestConsumer.java
+++ src/main/java/ch/ruyalabs/springkafkalab/consumer/PaymentRequestConsumer.java
@@ -1,8 +1,10 @@
 package ch.ruyalabs.springkafkalab.consumer;
 
 import ch.ruyalabs.springkafkalab.dto.PaymentDto;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -10,7 +12,7 @@ import org.springframework.stereotype.Component;
 public class PaymentRequestConsumer {
 
     @KafkaListener(topics = "${app.kafka.topics.payment-request}", groupId = "${app.kafka.consumer.payment-request.group-id}")
-    public void consume(PaymentDto paymentDto) {
+    public void consume(@Payload @Valid PaymentDto paymentDto) {
         log.info("Consumed PaymentDto from payment-request topic: {}", paymentDto);
     }
 }
