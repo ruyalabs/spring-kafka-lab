11dcff7d84ce5a293496c96fa997b8b771bf7b2c
ruyalabs <dev@ruyalabs.ch>
2025-06-22 13:33:36 +0200

    Initialize Spring Kafka project with Maven Wrapper and basic configurations.

---
+++ src/main/java/ch/ruyalabs/springkafkalab/SpringKafkaLabApplication.java
@@ -0,0 +1,13 @@
+package ch.ruyalabs.springkafkalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/ruyalabs/springkafkalab/SpringKafkaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ruyalabs.springkafkalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
